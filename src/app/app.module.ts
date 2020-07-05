cceb49e441097210f94c9136b6180d91fb1dedde
unknown <manishchavan016@gmail.com>
2020-07-05 12:33:18 +0530

    Add Auth Component and Customer Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { AngularRoutingModule } from './app-routing.module';
+import { CutomerHeaderComponent } from './cutomer-header/cutomer-header.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CutomerHeaderComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cutomer-header/cutomer-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cutomer-header',
+  templateUrl: './cutomer-header.component.html',
+  styleUrls: ['./cutomer-header.component.css']
+})
+export class CutomerHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
